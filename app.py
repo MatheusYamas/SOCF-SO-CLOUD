14968ab641f71ca5f6aa07b500d67e1a45089b46
Matheus Yamamoto Dias <101658885+MatheusYamas@users.noreply.github.com>
2025-11-10 23:35:26 +0000

    v1

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import platform
+import psutil
+from flask import Flask, jsonify, render_template
+
+APP = Flask(__name__)
+
+@APP.route('/')
+def index():
+    return render_template('index.html')
+
+@APP.route('/info')
+def get_info():
+    integrantes = ["Gustavo Tasca", "Matheus Yamamoto", "Mateus Roese", "Victor Ryuki"] 
+    return jsonify(integrantes=integrantes)
+
+@APP.route('/metricas')
+def get_metricas():
+    pid = os.getpid()
+
+    processo = psutil.Process(pid)
+    memoria_mb = processo.memory_info().rss / (1024 * 1024)
+    uso_cpu = processo.cpu_percent(interval=0.1)
+    so_nome = platform.system()
+    so_versao = platform.version()
+
+    metricas = {
+        "pid": pid,
+        "memoria_mb": round(memoria_mb, 2),
+        "uso_cpu_percent": uso_cpu,
+        "sistema_operacional": f"{so_nome} ({so_versao})"
+    }
+    return jsonify(metricas)
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0', port=8080)
